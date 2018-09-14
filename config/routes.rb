bec43c27327606801d8c36c25e46494f6cf64284
Jorge Gallegos <jorgegallegos403@gmail.com>
2018-09-14 02:04:48 +0000

    Add views for users show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'welcome#index'
-  get 'my_portfolio', to: "users#my_portfolio"
+  get 'my_portfolio' => "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
-  get 'my_friends', to: "users#my_friends"
-  
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
